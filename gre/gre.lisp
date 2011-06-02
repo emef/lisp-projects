dfb49bf5468619118dc1f031080e0726dbc1b3fe
matt forbes <ma@ttforbes.com>
2011-06-02 02:35:27 +0000

    making some progress

--- gre/gre.lisp
+++ gre/gre.lisp
@@ -7,13 +7,24 @@
 
 (setq *dispatch-table*
       `(,(create-prefix-dispatcher "/test" 'test-page)
-         ,(create-prefix-dispatcher "/about" 'about-page)))
+         ,(create-prefix-dispatcher "/about" 'about-page)
+         ,(create-prefix-dispatcher "/test/3" (handler-w-args "\/test\/(\\d+)" 'try-it))))
+         
+(defun handler-w-args (reg-uri fn)
+  `(lambda ()
+      (apply fn (scan-to-strings reg-uri (request-uri *request*)))))
+    
+(defmacro uri-let ())
+
+(defun try-it (str)
+  str)
+  
 
 (defun test-page ()
   (let ((name (parameter "name")))
     (if name
         (format nil "Hi, <b>~a</b>" name)
-        "Name: <form action='/test' method='get'><input type='text' name='name' /><input type='submit' name='submit' /></form>")))
+        "Name: <form action='' method='get'><input type='text' name='name' /><input type='submit' name='submit' /></form>")))
 
 (defun about-page ()
   "<h1>Hunchentoot Demo</h1>This is a very simple demonstration of the Hunchentoot webserver.")
